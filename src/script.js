605eb8cd73a312d40c92cff0ccaddb2f6c3e158f
Lucas Braga <lucasrbraga77@gmail.com>
2025-06-11 12:37:12 -0300

    mudei

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
 function mudaImagens(id){
-    const slime =document.querySelector('.imagem-slime');
+    const slime =document.querySelector('.slime');
     slime.src = `../Imagens/Rimuro-0${id}.webp`
 }
